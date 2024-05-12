0b587ede071bcf16d955054f6d992276ad4f1153
Klausfun <artem.nemurov451@gmail.com>
2024-05-12 21:27:56 +0300

    add connecting to the database

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	augventure "github.com/klausfun/Augventure"
 	"github.com/klausfun/Augventure/pkg/handler"
 	"github.com/klausfun/Augventure/pkg/repository"
 	"github.com/klausfun/Augventure/pkg/service"
+	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"log"
+	"os"
 )
 
 func main() {
@@ -15,7 +19,23 @@ func main() {
 		logrus.Fatalf("error initializing configs: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		logrus.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
