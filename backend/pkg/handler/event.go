93721f030225225ac3ab00afdf8ca6d6161944bf
Klausfun <artem.nemurov451@gmail.com>
2024-05-13 19:08:50 +0300

    add middleware

--- backend/pkg/handler/event.go
+++ backend/pkg/handler/event.go
@@ -1,9 +1,10 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func (h *Handler) createEvents(c *gin.Context) {
-
 }
 
 func (h *Handler) getAllEvents(c *gin.Context) {
--- backend/pkg/service/service.go
+++ backend/pkg/service/service.go
@@ -8,6 +8,7 @@ import (
 type Authorization interface {
 	CreateUser(user augventure.User) (int, error)
 	GenerateToken(username, password, email string) (string, error)
+	ParseToken(token string) (int, error)
 }
 
 type Event interface {
