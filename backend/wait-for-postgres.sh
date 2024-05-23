e37eddd6e9a505cbcf29f71e8b996e7cc51ced22
Klausfun <artem.nemurov451@gmail.com>
2024-05-23 15:01:04 +0300

    deleted the comments

--- backend/wait-for-postgres.sh
+++ backend/wait-for-postgres.sh
@@ -14,23 +14,3 @@ done
 
 >&2 echo "Postgres is up - executing command"
 exec $cmd
-##!/bin/sh
-## wait-for-postgres.sh
-#
-#set -e
-#
-#host="$1"
-#shift
-#cmd="$@"
-#
-#until PGPASSWORD=$DB_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
-#  >&2 echo "Postgres is unavailable - sleeping"
-#  sleep 1
-#done
-#
-#>&2 echo "Postgres is up - running migrations"
-#migrate -path ./schema -database "postgres://postgres:$DB_PASSWORD@$host:$DB_PORT/postgres?sslmode=disable" up
-#
-#>&2 echo "Migrations completed - executing command"
-#exec $cmd
-#
