e112e81e0364d9531b445938f5fe6196335939e2
Klausfun <artem.nemurov451@gmail.com>
2024-06-01 12:15:08 +0300

    added auth_test:signIn

--- backend/pkg/handler/auth_test.go
+++ backend/pkg/handler/auth_test.go
@@ -90,3 +90,91 @@ func TestHandler_signUp(t *testing.T) {
 		})
 	}
 }
+
+func TestHandler_signIn(t *testing.T) {
+	type mockBehavior func(s *mock_service.MockAuthorization, user signInInput)
+
+	testTable := []struct {
+		name                string
+		inputBody           string
+		inputUser           signInInput
+		mockBehavior        mockBehavior
+		expectedStatusCode  int
+		expectedRequestBody string
+	}{
+		{
+			name:      "OK",
+			inputBody: `{"email":"test@mail.ru", "password":"qwerty"}`,
+			inputUser: signInInput{
+				Email:    "test@mail.ru",
+				Password: "qwerty",
+			},
+			mockBehavior: func(s *mock_service.MockAuthorization, user signInInput) {
+				s.EXPECT().GenerateToken(user.Password, user.Email).Return(augventure.Author{
+					Name:     "",
+					Username: "",
+					Email:    "test@mail.ru",
+					PfpUrl:   "",
+					Bio:      "",
+				}, "1", nil)
+			},
+			expectedStatusCode:  200,
+			expectedRequestBody: `{"user":{"name":"","username":"","email":"test@mail.ru","pfp_url":"","bio":""},"token":"1"}`,
+		},
+		{
+			name:                "Empty Fields",
+			inputBody:           `{"password":"qwerty"}`,
+			mockBehavior:        func(s *mock_service.MockAuthorization, user signInInput) {},
+			expectedStatusCode:  400,
+			expectedRequestBody: `{"message":"invalid input body"}`,
+		},
+		{
+			name:      "Service Failure",
+			inputBody: `{"email":"test@mail.ru","password":"qwerty"}`,
+			inputUser: signInInput{
+				Email:    "test@mail.ru",
+				Password: "qwerty",
+			},
+			mockBehavior: func(s *mock_service.MockAuthorization, user signInInput) {
+				s.EXPECT().GenerateToken(user.Password, user.Email).Return(augventure.Author{
+					Name:     "",
+					Username: "",
+					Email:    "test@mail.ru",
+					PfpUrl:   "",
+					Bio:      "",
+				}, "1", errors.New("service failure"))
+			},
+			expectedStatusCode:  500,
+			expectedRequestBody: `{"message":"service failure"}`,
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			// Init Deps
+			c := gomock.NewController(t)
+			defer c.Finish()
+
+			auth := mock_service.NewMockAuthorization(c)
+			testCase.mockBehavior(auth, testCase.inputUser)
+
+			services := &service.Service{Authorization: auth}
+			handler := NewHandler(services)
+
+			// Test Server
+			r := gin.New()
+			r.POST("/signin", handler.signIn)
+
+			// Test Request
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest("POST", "/signin", bytes.NewBufferString(testCase.inputBody))
+
+			// Perform Request
+			r.ServeHTTP(w, req)
+
+			// Assert
+			assert.Equal(t, testCase.expectedStatusCode, w.Code)
+			assert.Equal(t, testCase.expectedRequestBody, w.Body.String())
+		})
+	}
+}
