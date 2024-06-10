134e029f0925ad100c0f1641ff367150ddd17834
Klausfun <artem.nemurov451@gmail.com>
2024-06-10 18:56:38 +0300

    fixed a bug

--- backend/pkg/handler/suggestions_test.go
+++ backend/pkg/handler/suggestions_test.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"bytes"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
 	augventure "github.com/klausfun/Augventure"
@@ -46,25 +47,35 @@ func TestHandler_getSuggestionsBySprintId(t *testing.T) {
 					},
 				}, nil)
 			},
-			expectedStatusCode: 200,
-			expectedRequestBody: `{"data":[{"id":1,"author_id":1,"sprint_id":1,
-			"author":{"name":"","username":"test","email":"test@mail.ru","pfp_url":"","bio":""},"content":"content","post_date":"","votes":0],}`,
+			expectedStatusCode:  200,
+			expectedRequestBody: `{"data":[{"id":1,"author_id":1,"sprint_id":1,"author":{"name":"","username":"test","email":"test@mail.ru","pfp_url":"","bio":""},"content":"content","post_date":"","votes":0}]}`,
 		},
-		//{
-		//	name:        "Service Failure",
-		//	inputUserId: 1,
-		//	mockBehavior: func(s *mock_service.MockSuggestion, userId int) {
-		//		s.EXPECT().GetById(userId).Return(augventure.Author{
-		//			Name:     "",
-		//			Username: "test",
-		//			Email:    "test@mail.ru",
-		//			PfpUrl:   "",
-		//			Bio:      "",
-		//		}, errors.New("service failure"))
-		//	},
-		//	expectedStatusCode:  500,
-		//	expectedRequestBody: `{"message":"service failure"}`,
-		//},
+		{
+			name:          "Service Failure",
+			inputBody:     `{"sprint_id":1}`,
+			inputSprintId: 1,
+			mockBehavior: func(s *mock_service.MockSuggestion, sprintId int) {
+				s.EXPECT().GetBySprintId(sprintId).Return([]augventure.FilterSuggestions{
+					{
+						Id:       1,
+						AuthorId: 1,
+						SprintId: 1,
+						Author: augventure.Author{
+							Name:     "",
+							Username: "test",
+							Email:    "test@mail.ru",
+							PfpUrl:   "",
+							Bio:      "",
+						},
+						Content:  "content",
+						PostDate: "",
+						Votes:    0,
+					},
+				}, errors.New("service failure"))
+			},
+			expectedStatusCode:  500,
+			expectedRequestBody: `{"message":"service failure"}`,
+		},
 	}
 
 	for _, testCase := range testTable {
@@ -83,7 +94,7 @@ func TestHandler_getSuggestionsBySprintId(t *testing.T) {
 			r := gin.New()
 			r.POST("/api/suggestions/get", func(c *gin.Context) {
 				c.Set(userCtx, testCase.inputSprintId)
-				handler.getUser(c)
+				handler.getSuggestionsBySprintId(c)
 			})
 
 			// Test Request
